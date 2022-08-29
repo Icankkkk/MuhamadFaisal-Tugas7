3e5bb44db16c56a4e9eba5a1e8b10505e8c917f2
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2022-08-29 15:10:48 +0800

    Optimasi AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] private List<AudioSource> _bgmAudioSource;
+    [SerializeField] private AudioSource _bgmAudioSource;
+
     public bool isAudioEnabled = false;
-     private static AudioManager _instance;
+    private static AudioManager _instance;
 
     public static AudioManager Instance { get { return _instance; } }
-    private void Awake() 
-    {   
-       if (_instance != null && _instance != this)
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
-        } else {
+        }
+        else
+        {
             _instance = this;
             DontDestroyOnLoad(this);
         }
     }
+
     public void PlayBgm()
     {
-        _bgmAudioSource[Random.Range(0, _bgmAudioSource.Count)].Play();
+        _bgmAudioSource.clip = Resources.Load<AudioClip>("BGM/BGM_0" + Random.Range(1, 5));
+        _bgmAudioSource.Play();
     }
 }
--- Assets/Scripts/MenuLauncher.cs
+++ Assets/Scripts/MenuLauncher.cs
@@ -24,7 +24,6 @@ public class MenuLauncher : MonoBehaviour
         }
         private void OnWithoutAudioButtonClicked()
         {
-            
             AudioManager.Instance.isAudioEnabled = false;
             SceneManager.LoadScene("Game");
         }
