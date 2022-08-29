3461d9e8335fc7c1c790cb32b5fecd1561028eeb
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2022-08-29 16:59:53 +0800

    Convert instantiate to object pool

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
-    public float upForce = 1f;
-    public float sideForce = .1f;
-    void Start()
+    [SerializeField] private float upForce = 1f;
+    [SerializeField] private float sideForce = .1f;
+    [SerializeField] private float TimeToDeactive = 3;
+
+    private float timer = 0;
+    public void SpawnObject(Vector3 pos)
     {
+        timer = 0;
+
+        transform.position = pos;
+
         float xForce = Random.Range(-sideForce, sideForce);
         float yForce = Random.Range(upForce/2f, upForce);
         float zForce = Random.Range(-sideForce, sideForce);
@@ -15,5 +22,19 @@ public class Mushroom : MonoBehaviour
         Vector3 force = new Vector3 (xForce, yForce, zForce);
 
         GetComponent<Rigidbody>().velocity = force;
+
+        gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > TimeToDeactive) DeActive();
+    }
+
+    public void DeActive()
+    {
+        gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/MushroomSpawner.cs
+++ Assets/Scripts/MushroomSpawner.cs
@@ -4,9 +4,48 @@ using UnityEngine;
 
 public class MushroomSpawner : MonoBehaviour
 {
-    public GameObject objPrefab;
-    
+   [SerializeField] private Mushroom objPrefab;
+    [SerializeField] private int size;
+
+    private List<Mushroom> musroomPool;
+    private Vector3 spawnPos;
+
+    private void Start()
+    {
+        spawnPos = new Vector3(transform.position.x, 10, transform.position.z);
+
+        musroomPool = new List<Mushroom>();
+
+        for (int i = 0; i < size; i++)
+        {
+            InstantiateNewOne(objPrefab, musroomPool);
+        }
+    }
+
     void FixedUpdate(){
-        Instantiate(objPrefab, transform.position, Quaternion.identity);
+        SpawnObject(musroomPool, spawnPos);
+    }
+
+    private void InstantiateNewOne(Mushroom obj, List<Mushroom> pool)
+    {
+        Mushroom item = Instantiate(obj);
+        item.DeActive();
+
+        pool.Add(item);
+    }
+
+    private void SpawnObject(List<Mushroom> pool, Vector3 pos)
+    {
+        foreach (Mushroom item in pool)
+        {
+            if (!item.gameObject.activeInHierarchy)
+            {
+                item.SpawnObject(pos);
+                return;
+            }
+        }
+
+        InstantiateNewOne(objPrefab, pool);
+        pool[pool.Count - 1].SpawnObject(pos);
     }
 }
